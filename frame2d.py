91748e819b6708affb6d4af1a8388d9137def31b
mskz1 <mskz.tsuji@me.com>
2018-12-16 17:35:27 +0900

    修正

--- frame2d.py
+++ frame2d.py
@@ -1,16 +1,121 @@
+import matplotlib.pyplot as plt
+import matplotlib.lines as ml
+import matplotlib.patches as mp
+import numpy as np
+from matplotlib.path import Path
+from matplotlib.patches import PathPatch
+
+
 class Model:
     pass
 
 
 class Node:
-    def __init__(self, x=0, y=0):
+    def __init__(self, tag=0, x=0, y=0, loaded=False, supported=False):
+        self._tag = tag
         self._x = x
         self._y = y
+        self._loaded = loaded
+        self._supported = supported
 
     @property
     def x(self):
         return self._x
 
+    @x.setter
+    def x(self, x):
+        self._x = x
+
     @property
     def y(self):
         return self._y
+
+    @y.setter
+    def y(self, y):
+        self._y = y
+
+    @property
+    def tag(self):
+        return self._tag
+
+    @tag.setter
+    def tag(self, tag):
+        self._tag = tag
+
+    def plot(self, ax, disp_tag=False):
+        ct_style = dict(color='k', linestyle='solid', linewidth=2., fill=True)
+        text_style = dict(textcoords='offset points', color='k', size='x-small')
+
+        ax.add_patch(mp.Circle((self._x, self._y), radius=1, **ct_style))
+        if disp_tag:
+            ax.annotate(str(self._tag), (self._x, self._y), xytext=(3, 3), **text_style)
+
+    def plot_support_pin(self, ax):
+        size = 50
+        ox = self._x
+        oy = self._y
+        vertices = []
+        codes = []
+        codes = [Path.MOVETO] + [Path.LINETO] * 2 + [Path.CLOSEPOLY]
+        vertices = [(ox, oy), (ox + size / 2, oy - size * 1.73 / 2), (ox - size / 2, oy - size * 1.73 / 2), (0, 0)]
+
+        vertices = np.array(vertices, float)
+        path = Path(vertices, codes)
+        pathpatch = PathPatch(path, facecolor='None', edgecolor='k')
+        ax.add_patch(pathpatch)
+
+    def plot_support_roller(self, ax):
+        size = 50
+        ox = self._x
+        oy = self._y
+        vertices = []
+        codes = []
+        codes = [Path.MOVETO] + [Path.LINETO] * 2 + [Path.CLOSEPOLY]
+        vertices = [(ox, oy), (ox + size / 2, oy - size * 1.73 / 2), (ox - size / 2, oy - size * 1.73 / 2), (0, 0)]
+
+        codes += [Path.MOVETO] + [Path.LINETO] * 1 + [Path.CLOSEPOLY]
+        vertices += [(ox + size / 2, oy - size * 1.73 / 2 - size / 4), (ox - size / 2, oy - size * 1.73 / 2 - size / 4),
+                     (0, 0)]
+        vertices = np.array(vertices, float)
+        path = Path(vertices, codes)
+        pathpatch = PathPatch(path, facecolor='None', edgecolor='k')
+        ax.add_patch(pathpatch)
+
+
+class Element:
+    def __init__(self, tag=0, node1=0, node2=0):
+        self._tag = tag
+        self._node1 = node1
+        self._node2 = node2
+
+    @property
+    def node1(self):
+        return self._node1
+
+    @node1.setter
+    def node1(self, node1):
+        self._node1 = node1
+
+    @property
+    def node2(self):
+        return self._node2
+
+    @node2.setter
+    def node2(self, node2):
+        self._node2 = node2
+
+    @property
+    def tag(self):
+        return self._tag
+
+    @tag.setter
+    def tag(self, tag):
+        self._tag = tag
+
+    def plot(self, ax, node1, node2, disp_tag=False):
+        line_style = dict(color='b', linestyle='solid', linewidth=2., marker='.')
+        text_style = dict(textcoords='offset points', color='b', size='x-small')
+
+        ax.add_line(ml.Line2D((node1.x, node2.x), (node1.y, node2.y), **line_style))
+        if disp_tag:
+            ax.annotate(str(self._tag), ((node1.x + node2.x) / 2, (node1.y + node2.y) / 2), xytext=(3, 3), **text_style)
--- frame2d_test.py
+++ frame2d_test.py
@@ -1,12 +1,77 @@
+import matplotlib.pyplot as plt
 from frame2d import Model
-from frame2d import Node
+from frame2d import Node, Element
+from pytest import approx
+import pytest
+
 
 def test_node():
-    n1 = Node(0,0)
-    n2 = Node(1000,0)
+    n1 = Node(x=0, y=0)
+    n2 = Node(x=1000, y=0)
+    n3 = Node()
+    n3.x = 400.
+    n3.y = -200
+    n3.tag = 5
     assert n1.x == 0
     assert n2.x == 1000
     assert n2.y == 0
+    assert n3.x == 400
+    assert n3.y == -200
+    assert n3.tag == 5
 
 
+def test_element():
+    e1 = Element()
+    e1.tag = 1
+    e1.node1 = 1
+    e1.node2 = 2
+    assert e1.tag == 1
+    assert e1.node1 == 1
+
+
+@pytest.mark.skip
+def test_plot_node():
+    nodes = [Node(1, 0, 0), Node(2, 1000, 0), Node(3, 2000, 1000)]
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+
+    for n in nodes:
+        n.plot(ax, disp_tag=True)
+
+    plt.axis('equal')
+    plt.show()
+
+@pytest.mark.skip
+def test_plot_element():
+    n1 = Node(1, 0, 0)
+    n2 = Node(2, 1000, 0)
+    n3 = Node(3, 2000, 1000)
+    nodes = [n1, n2, n3]
+    e1 = Element(1, 1, 2)
+    e2 = Element(2, 2, 3)
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+
+    e1.plot(ax, n1, n2, disp_tag=True)
+    e2.plot(ax, n2, n3, disp_tag=True)
+
+    plt.axis('equal')
+    plt.show()
+
+def test_plot_support_fig():
+    n1 = Node(1, 0, 0)
+    n2 = Node(2, 1000, 0)
+    n3 = Node(3, 2000, 1000)
+
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+
+    # pin support
+    n1.plot_support_pin(ax)
+
+    n2.plot(ax)
+    n2.plot_support_roller(ax)
+    n3.plot(ax)
+    plt.axis('equal')
+    plt.show()
 
